bcacaa4cd2b3efb26fd8ea0d3ca0f7575812dcca
Daniela Rios <danirioscampelo@gmail.com>
2022-10-18 21:29:40 -0300

    todo back end feito + view de index e show da model list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/delete'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
