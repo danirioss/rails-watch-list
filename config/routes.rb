f86ed586324ec49735d9946618e5336058fbc624
Daniela Rios <danirioscampelo@gmail.com>
2022-10-22 12:50:13 -0300

    root criada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
   resources :lists, only: [:index, :show, :new, :create, :edit, :update] do
     resources :bookmarks, only: [:create, :new]
   end
