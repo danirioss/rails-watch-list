8247fccc5228814cc81f08c5e5a2b371d6b4ff32
Daniela Rios <danirioscampelo@gmail.com>
2022-10-22 12:56:44 -0300

    ätualizacao da database so com movies - listas e bookmarks zerados

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,3 @@ Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s f
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-
-List.create(name: "classic")
-List.create(name: "comedy")
-List.create(name: "drama")
-List.create(name: "marvel e afins")
-
-Bookmark.create(comment: "bestinha mas legal!!!!", movie_id: 1, list_id: 4 )
-Bookmark.create(comment: "chorei horrores", movie_id: 2, list_id: 2 )
-Bookmark.create(comment: "o classico dos classicos", movie_id: 3, list_id: 1 )
-Bookmark.create(comment: "tiros acoes e muito sangue uhu", movie_id: 4, list_id: 2 )
